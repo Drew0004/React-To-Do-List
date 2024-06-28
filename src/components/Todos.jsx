7f4cb3f97528084e611ec906e0154a5f3ddd7f82
Drew0004 <drdrewgraphic@gmail.com>
2024-06-28 19:45:33 +0200

    Add remove todo function from array

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -32,6 +32,11 @@ const Todos = () => {
         setTodos(updatedToDos)
     }
 
+    const removeToDo = (id) =>{
+        const removedTodo = todos.filter((el) => el.id !== id)
+        setTodos(removedTodo)
+    }
+
     return (
         <>
             <form className="row justify-content-between align-items-center my-5" onSubmit={handleNewTask}>
@@ -45,15 +50,18 @@ const Todos = () => {
             <div className='todos-box rounded-5 p-5'>
                 {
                     todos.length === 0 ? 
-                    <h2 className='my-font text-white'>Sembra non ci siano Tasks...</h2> : 
+                    <h3 className='my-font text-white'>Sembra non ci siano Tasks per ora...</h3> : 
                     todos.map((elem)=>{
                         return (
                             <>
-                                <div className="row justify-content-between">
-                                    <div className="col-10">
+                                <div className="row justify-content-between align-items-center border-bottom py-3">
+                                    <div className="col-8">
                                         <h5 className={`fw-bold ${elem.done === true ? 'task-done' : 'text-white'}`} key={elem.id}>{elem.task}</h5>
                                     </div>
-                                    <button onClick={()=>handleDoneStatus(elem.id)} className='col-1 my-button rounded-5'><i class="fa-solid fa-check text-white"></i></button>
+                                    <div className='col-2 d-flex align-items-center'>
+                                        <button onClick={()=>handleDoneStatus(elem.id)} className='px-3 py-2 my-button rounded-5'><i class="fa-solid fa-check text-white"></i></button>
+                                        <button onClick={()=>removeToDo(elem.id)} className='px-3 py-2 ms-5 my-delete-button rounded-5'><i class="fa-solid fa-trash text-white"></i></button>
+                                    </div>
                                 </div>
                             </>
                         )
